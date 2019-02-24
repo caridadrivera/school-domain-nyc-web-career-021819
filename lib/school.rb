f3843140e9c740b2d91a31c597aa138c0b3e6f71
Caridad Rivera <ccrive10@gmail.com>
2019-02-24 18:11:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School #the school class is a considered a model in the domain
+
+
+  def initialize(school)
+    @school = school
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+#setter
+def grade=(grade)
+  @grade = grade
+end
+
+#getter
+def grade
+  @grade
+end
+
+end
